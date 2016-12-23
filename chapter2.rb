0a2f2a0bd9bcb718da087cd715c20c1e11c976dc
NickBrath <brathwaiten16@berkeley.edu>
2016-12-23 15:40:04 -0800

    First commit

---
+++ chapter2.rb
@@ -0,0 +1,23 @@
+puts 1+2 
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0 
+puts 9.0 / 2.0
+"hours in a year"
+puts 24 * 365
+"minutes in a decade"
+puts 60 * 24*365 * 10
+"age in seconds"
+puts 60 * 60 * 24 * 365 * 18
+
+puts 'Hello World'
+puts ''
+puts 'Good-bye'
+puts 'I like' + ' apple pie'
+
+echo "# cs61aPrepCourse" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/NickBrath/cs61aPrepCourse.git
+git push -u origin master
